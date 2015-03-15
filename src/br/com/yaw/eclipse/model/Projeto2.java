e690d9d56e72bc74712cc87ada9fd569189904c6
François Jr <Franois Jr@k45vm.lan>
2015-03-14 22:31:45 -0300

    Adicionando a classe com egit

---
+++ src/br/com/yaw/eclipse/model/Projeto2.java
@@ -0,0 +1,14 @@
+package br.com.yaw.eclipse.model;
+
+public class Projeto2 {
+
+	private String descricao;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
